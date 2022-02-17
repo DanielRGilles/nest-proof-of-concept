a0a2bf60eae8154bcf6ed80fad06a72a90cb9f18
Zack Lyon <zack.j.lyon@gmail.com>
2022-02-17 13:29:06 -0800

    small changes and deploy on heroku

--- src/db/db.module.ts
+++ src/db/db.module.ts
@@ -5,20 +5,19 @@ import { Cats } from 'src/cats/models/post.entity';
 import { Dogs } from 'src/dogs/models/post.entity';
 
 @Module({
-    imports: [
-        ConfigModule.forRoot({ isGlobal: true }),
-        TypeOrmModule.forRoot({
-            type: 'postgres',
-            host: process.env.HOST,
-            port: parseInt(<string>process.env.POSTGRES_PORT),
-            username: process.env.POSTGRES_USER,
-            password: process.env.POSTGRES_PASSWORD,
-            database: process.env.POSTGRES_DATABASE,
-            autoLoadEntities: true,
-            synchronize: true,
-            entities: [Dogs, Cats]
-
-        }),
-    ],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      // host: process.env.HOST,
+      // port: parseInt(<string>process.env.POSTGRES_PORT),
+      // username: process.env.POSTGRES_USER,
+      // password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
+      entities: [Dogs, Cats],
+    }),
+  ],
 })
 export class DbModule {}
